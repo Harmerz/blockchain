4649d848413f8cc4cfc472a3f45c7ad8a13134b7
Haikal Hilmi <haikal6255hilmi@gmail.com>
2023-12-17 18:04:22 +0700

    feat : Product Function

---
+++ contracts/SupplyChain.sol
@@ -0,0 +1,106 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+/** 
+ * @title Ballot
+ * @dev Implements voting process along with vote delegation
+ */
+contract SupplyChain {
+    struct ProductRecord {
+        uint256 shipmentID;
+        uint256 weight;
+        uint256 timestamp;
+    }
+
+    struct Inventory {
+        uint256 productID;
+        uint256 totalWeight;
+        ProductRecord[] productRecords;
+    }
+
+    struct Person {
+        address userAddress;
+        string name;
+        string occupation;
+        string location;
+        Inventory[] inventory;
+        ProductRecord[] sendShipment;
+    }
+
+    struct Shipment {
+        address fromAddress;
+        address toAddress;
+        uint8 shipmentID;
+        uint256 weight;
+        uint256 productID;
+        ProductRecord[] productRecords;
+    }
+
+    struct Product {
+        uint256 productID;
+        uint256 idBefore;
+        string name;
+        uint8 changedPercent;
+        string Type;
+        string description;
+    }
+
+    mapping(uint256 => Product) public products;
+    uint256 public nextProductId = 1;
+
+    function createProduct(
+        uint256 _idBefore,
+        string memory _name,
+        uint8 _changedPercent,
+        string memory _type,
+        string memory _description
+    ) external {
+        Product memory newProduct = Product({
+            productID: nextProductId,
+            idBefore: _idBefore,
+            name: _name,
+            changedPercent: _changedPercent,
+            Type: _type,
+            description: _description
+        });
+
+        products[nextProductId] = newProduct;
+
+        nextProductId++;
+    }
+
+     function updateProduct(
+        uint256 _productId,
+        uint256 _idBefore,
+        string memory _name,
+        uint8 _changedPercent,
+        string memory _type,
+        string memory _description
+    ) external {
+        require(_productId > 0 && _productId < nextProductId, "Invalid product ID");
+
+        Product storage productToUpdate = products[_productId];
+        productToUpdate.idBefore = _idBefore;
+        productToUpdate.name = _name;
+        productToUpdate.changedPercent = _changedPercent;
+        productToUpdate.Type = _type;
+        productToUpdate.description = _description;
+    }
+
+    function getAllProducts() external view returns (Product[] memory) {
+        Product[] memory allProducts = new Product[](nextProductId - 1);
+
+        for (uint256 i = 1; i < nextProductId; i++) {
+            allProducts[i - 1] = products[i];
+        }
+
+        return allProducts;
+    }
+
+    function getProductById(uint256 _productId) external view returns (Product memory) {
+        require(_productId > 0 && _productId < nextProductId, "Invalid product ID");
+        return products[_productId];
+    }
+
+}
---
+++ test/product.test.js
@@ -0,0 +1,70 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("SupplyChain", function () {
+  let SupplyChain;
+  let supplyChain;
+  const initialProductId = 1;
+
+  beforeEach(async function () {
+    SupplyChain = await ethers.getContractFactory("SupplyChain");
+    supplyChain = await SupplyChain.deploy();
+  });
+
+  it("should create a product", async function () {
+    await supplyChain.createProduct(0, "Product 1", 10, "Type A", "Description A");
+
+    const product = await supplyChain.products(initialProductId);
+
+    expect(product.productID).to.equal(initialProductId);
+    expect(product.idBefore).to.equal(0);
+    expect(product.name).to.equal("Product 1");
+    expect(product.changedPercent).to.equal(10);
+    expect(product.Type).to.equal("Type A");
+    expect(product.description).to.equal("Description A");
+  });
+
+  it("should update a product", async function () {
+    await supplyChain.createProduct(0, "Product 1", 10, "Type A", "Description A");
+
+    const updatedValues = {
+      idBefore: 1,
+      name: "Updated Product",
+      changedPercent: 20,
+      Type: "Type B",
+      description: "Updated Description",
+    };
+
+    await supplyChain.updateProduct(initialProductId, ...Object.values(updatedValues));
+
+    const updatedProduct = await supplyChain.products(initialProductId);
+
+    expect(updatedProduct.productID).to.equal(initialProductId);
+    expect(updatedProduct.idBefore).to.equal(updatedValues.idBefore);
+    expect(updatedProduct.name).to.equal(updatedValues.name);
+    expect(updatedProduct.changedPercent).to.equal(updatedValues.changedPercent);
+    expect(updatedProduct.Type).to.equal(updatedValues.Type);
+    expect(updatedProduct.description).to.equal(updatedValues.description);
+  });
+
+  it("should get all products", async function () {
+    await supplyChain.createProduct(0, "Product 1", 10, "Type A", "Description A");
+    await supplyChain.createProduct(1, "Product 2", 15, "Type B", "Description B");
+
+    const allProducts = await supplyChain.getAllProducts();
+    console.log(allProducts[0])
+
+    expect(allProducts.length).to.equal(2);
+
+    expect(allProducts[0].name).to.equal("Product 1");
+    expect(allProducts[1].name).to.equal("Product 2");
+  });
+
+  it("should get product by ID", async function () {
+    await supplyChain.createProduct(0, "Product 1", 10, "Type A", "Description A");
+
+    const product = await supplyChain.getProductById(initialProductId);
+
+    expect(product.name).to.equal("Product 1");
+  });
+});
